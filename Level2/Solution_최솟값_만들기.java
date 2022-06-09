ef820cc89e0792cd40ff17ec8303f5de64932bed
HoJin Park <85658845+hojin7769@users.noreply.github.com>
2022-06-09 11:09:23 +0900

    CodingTest Level2 Start
    코테 레벨2 시작 하며 커밋

---
+++ Level2/Solution_최솟값_만들기.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution_최솟값_만들기 {
+
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        Integer[] B2 = new Integer[B.length];
+
+        for (int i  = 0 ; i < B.length ; i++){
+            B2[i] = B[i];
+        }
+
+
+
+        Arrays.sort(A);
+        Arrays.sort(B2, Collections.reverseOrder());
+
+         for (int i = 0 ; i < B.length; i++){
+             answer += A[i] * B2[i];
+         }
+
+
+
+        return answer;
+    }
+}
