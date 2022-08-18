fb4f95670a3b9b0e7a420747d0170b51b155486f
hojin <sungsill150@gmail.com>
2022-08-18 21:10:47 +0900

    오늘코딩 테스트 커밋

---
+++ bakjun/BJ10171.java
@@ -0,0 +1,12 @@
+package bakjun;
+
+public class BJ10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+
+    }
+}
