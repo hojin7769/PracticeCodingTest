2c54e22e7e966719457099c61d917642585505ef
hojin <sungsill150@gmail.com>
2022-10-16 21:58:50 +0900

    ㅆ

---
+++ was-practice/src/test/java/org/example/GradeCalculator.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.List;
+
+public class GradeCalculator {
+
+    private final List<Course> courses;
+
+    public GradeCalculator(List<Course> courses) {
+
+        this.courses = courses;
+
+    }
+
+    public double calculateGrade() {
+        double multipliedCreditAndCourseGrade = 0;
+        for (Course course : courses) {
+            multipliedCreditAndCourseGrade += course.getCredit() * course.getGradeToNumber();
+        }
+
+        int totalCompletedCreadit = courses.stream()
+                .mapToInt(Course::getCredit)
+                .sum();
+
+        return multipliedCreditAndCourseGrade/totalCompletedCreadit;
+    }
+}
